15840c37eb7612208038eb12f2512776e48ab52e
polojerry <polojerry4@gmail.com>
2020-03-06 12:55:17 +0300

    Created a Person Model and A Person Data Access Object (DAO)

---
+++ src/main/java/com/example/demo/dao/PersonDao.java
@@ -0,0 +1,17 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.Person;
+
+import java.util.UUID;
+
+public interface PersonDao {
+
+    //Create Person with an id
+    int insertPerson(UUID id, Person person);
+
+    //Create person with random id
+    default int addPerson(Person person){
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+}
